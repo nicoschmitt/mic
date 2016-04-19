a3c85c14353db72c6d60b03736c6d6d006015963
Nicolas Schmitt <nicolas.schmitt@outlook.fr>
2016-04-19 21:36:59 +0200

    remove throng, switch to pm2

--- server.js
+++ server.js
@@ -1,43 +1,27 @@
 require('dotenv').config({silent: true});
-
-var env = process.env.NODE_ENV || "development";
-if (env == "production") {
-    var throng = require("throng");
-    var workers = process.env.WEB_CONCURRENCY || 1;
-    throng({
-        workers: workers,
-        start: worker
-    });
-} else {
-    worker();    
-}
-
-function worker(id) {
-    if (id) console.log("Start worker " + id);
     
-    var path = require('path');
-    var compression = require("compression");
+var path = require('path');
+var compression = require("compression");
 
-    var mongoose = require("mongoose");
-    mongoose.connect(process.env.MONGO_URI);
+var mongoose = require("mongoose");
+mongoose.connect(process.env.MONGO_URI);
 
-    var express = require('express');
-    var bodyParser = require('body-parser');
-    var app = express();
+var express = require('express');
+var bodyParser = require('body-parser');
+var app = express();
 
-    app.use(bodyParser.urlencoded({ extended: true }));
-    app.use(bodyParser.json());
-    app.set("etag", false);
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+app.set("etag", false);
 
-    var myserver = require("./server/start-http");
-    myserver.forceHttps(app);
+var myserver = require("./server/start-http");
+myserver.forceHttps(app);
 
-    require("./server/auth/auth-config").register(app);
+require("./server/auth/auth-config").register(app);
 
-    require('./server/routes').register(app);
+require('./server/routes').register(app);
 
-    app.use(compression());
-    app.use(express.static(path.resolve(__dirname, 'client')));
+app.use(compression());
+app.use(express.static(path.resolve(__dirname, 'client')));
 
-    myserver.start(app);
-}
+myserver.start(app);
