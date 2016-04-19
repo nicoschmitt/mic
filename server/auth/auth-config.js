3afbf862e720223f5a2ef8c9722367f78cfb7a1b
Nicolas Schmitt <nicolas.schmitt@outlook.fr>
2016-04-19 22:18:26 +0200

    remove AzureAD logging

--- server/auth/auth-config.js
+++ server/auth/auth-config.js
@@ -12,7 +12,8 @@
         passport.use(new OIDCBearerStrategy({
             "identityMetadata": "https://login.microsoftonline.com/common/v2.0/.well-known/openid-configuration",
             "audience": process.env.MS_APP_ID,
-            "validateIssuer": false
+            "validateIssuer": false,
+            "loggingLevel": "error"
         }, function (token, done) {
             return done(null, token, null);
         }));
