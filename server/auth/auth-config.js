c0def74becb896d7880e836d8c83f5881993d4da
Nicolas Schmitt <nicolas.schmitt@outlook.fr>
2016-07-19 23:18:01 +0200

    update lib

--- server/auth/auth-config.js
+++ server/auth/auth-config.js
@@ -4,12 +4,14 @@
     module.exports.register = function(app) {
 
         var passport = require('passport');
-        var OIDCBearerStrategy = require('passport-azure-ad').BearerStrategy;
+        var BearerStrategy = require('passport-azure-ad').BearerStrategy;
         var SimpleStrategy = require('./passport-simple').Strategy;
 
         app.use(passport.initialize());
+
         passport.use(new SimpleStrategy());
-        passport.use(new OIDCBearerStrategy({
+
+        passport.use(new BearerStrategy({
             "identityMetadata": "https://login.microsoftonline.com/common/v2.0/.well-known/openid-configuration",
             "audience": process.env.MS_APP_ID,
             "validateIssuer": false,
